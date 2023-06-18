8077131f486590b24d04df943434f9e9c23c800e
Дарья Виноградова <kapitoshkaizu@yandex.ru>
2023-06-18 11:42:22 +0300

    Commit 18/06/23-4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wofg+9jlt*37$64_kvnv1cxs%_r)1n8a*so_c)twjz8v1ymtt$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
